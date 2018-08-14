e05daa3c0d70a47575673af01fb265dbb4e75dc2
Jesse Smith <JesseSmith3rd@outlook.com>
2018-08-14 07:30:43 -0400

    correct syntax errors

--- logic.js
+++ logic.js
@@ -1,14 +1,14 @@
-/* Get our Elements*/
+/* Get Our Elements */
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider')
+const ranges = player.querySelectorAll('.player__slider');
 
-/*build out functions*/
-functions togglePlay() {
+/* Build out functions */
+function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
   video[method]();
 }
@@ -20,14 +20,14 @@ function updateButton() {
 }
 
 function skip() {
-  video.currentTime += parseFloat(this.dataset.skip);
+ video.currentTime += parseFloat(this.dataset.skip);
 }
 
 function handleRangeUpdate() {
   video[this.name] = this.value;
 }
 
-function handleProgress(){
+function handleProgress() {
   const percent = (video.currentTime / video.duration) * 100;
   progressBar.style.flexBasis = `${percent}%`;
 }
@@ -37,16 +37,16 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
-/*Set up the event listeners*/
+/* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
-skipButtons.forEach(button => button.addEventListener('click',skip));
+skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('mouse', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
 let mousedown = false;
 progress.addEventListener('click', scrub);
