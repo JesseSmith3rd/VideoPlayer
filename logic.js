51ad68e252c55f1a1829e67f4feb61040756179a
Jesse Smith <JesseSmith3rd@outlook.com>
2018-08-14 06:53:06 -0400

    set up function for update button

--- logic.js
+++ logic.js
@@ -12,3 +12,9 @@ functions togglePlay() {
   const method = video.paused ? 'play' : 'pause';
   video[method]();
 }
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
+  toggle.textContent = icon;
+}
