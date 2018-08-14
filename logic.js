945ecb6853bdaef1fe69612c2efb5178f2869449
Jesse Smith <JesseSmith3rd@outlook.com>
2018-08-14 07:16:30 -0400

    set up event listeners

--- logic.js
+++ logic.js
@@ -38,3 +38,18 @@ function scrub(e) {
 }
 
 /*Set up the event listeners*/
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click',skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mouse', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
